1a8a059f5c0b47276fcda0f4d8524dc728fa01d1
Gregory Brunner <gbrunner@esri.com>
2014-10-29 20:00:08 -0500

    The SINGS archival proposal we put together
    The SINGS archival proposal we put together

---
+++ SINGS_ARCHIVAL_PROPOSAL/greg_ARproposal.cs
@@ -0,0 +1,93 @@
+Proposal.version= 6.0
+Proposal.userName= singsong
+Proposal.pid= 40773
+Proposal.proposalType= archive
+Proposal.isTargetOfOpportunity= false
+Proposal.isThesis= true
+Proposal.requestedDollars= 96,765.00
+#------------------------- Title -------------------------------------
+Proposal.title= \
+     Mapping Molecular Hydrogen Excitation and Mass in Nearby Galaxies from \
+     the SINGS Archive
+
+#------------------------- Science Categories ------------------------
+Proposal.sciCategory.extraGalactic= nearby galaxies (z<0.05, v_sys<15,000 km/s)
+#------------------------- Science Justification File ----------------
+Proposal.ScienceJustification.File= /Users/greg/Desktop/SINGS_archival_proposal/greg_SINGS_AR_ks_v6.pdf
+#------------------------- PI Information ----------------------------
+Proposal.pi.firstName= Gregory
+Proposal.pi.lastName= Brunner
+Proposal.pi.middle= J.
+Proposal.pi.street.1= 6100 S. Main St.
+Proposal.pi.city= Houston
+Proposal.pi.state= Texas
+Proposal.pi.country= United States
+Proposal.pi.zip= 77005
+Proposal.pi.phone= 713-348-3348
+Proposal.pi.fax= 713-348-5143
+Proposal.pi.email= gbrunner@rice.edu
+Proposal.pi.dept= Physics and Astronomy
+Proposal.pi.institute= Rice University
+#------------------------- Contact Information -----------------------
+Proposal.contact.firstName= Gregory
+Proposal.contact.lastName= Brunner
+Proposal.contact.middle= J.
+Proposal.contact.street.1= 6100 S. Main St.
+Proposal.contact.city= Houston
+Proposal.contact.state= Texas
+Proposal.contact.country= United States
+Proposal.contact.zip= 77005
+Proposal.contact.phone= 713-348-3348
+Proposal.contact.fax= 713-348-5143
+Proposal.contact.email= gbrunner@rice.edu
+Proposal.contact.dept= Physics and Astronomy
+Proposal.contact.institute= Rice University
+#------------------------- Co-Investigator Information ---------------
+Proposal.coi.count= 4
+Proposal.coi.0.firstName= Reginald
+Proposal.coi.0.lastName= Dufour
+Proposal.coi.0.institute= Rice University
+Proposal.coi.0.email= rjd@rice.edu
+Proposal.coi.1.firstName= Kartik
+Proposal.coi.1.lastName= Sheth
+Proposal.coi.1.institute= Spitzer Science Center
+Proposal.coi.1.email= kartik@astro.caltech.edu
+Proposal.coi.2.firstName= Stuart 
+Proposal.coi.2.lastName= Vogel
+Proposal.coi.2.institute= University of Maryland
+Proposal.coi.2.email= vogel@astro.umd.edu
+Proposal.coi.3.firstName= Mark
+Proposal.coi.3.lastName= Wolfire
+Proposal.coi.3.institute= University of Maryland
+Proposal.coi.3.email= mwolfire@astro.umd.edu
+#------------------------- Abstract ----------------------------------
+Proposal.abstract= \
+     Molecular hydrogen (H2) is inherently connected with the evolution of a\
+     \ galaxy through a variety of processes.  In galaxies, H2 traces star fo\
+     rmation in nuclear regions, near the ends of bars in barred spirals, an\
+     d along spiral arms where star formation is triggered by the passage of\
+     \ a spiral density wave. The SINGS legacy program has used the Spitzer I\
+     nfrared Spectrograph (IRS) long-low module (LL; 14 - 38 microns) in spe\
+     ctral mapping mode to map large strips across 65 galaxies ranging in Hu\
+     bble type, star-formation rate, and nuclear activity.   We propose to u\
+     se the SINGS LL data cubes to map the spatial distribution of warm (T =\
+     \ 100 - 300 K) H2, traced by the H2 S(0) (28.2 micron) and H2 S(1) (17.0\
+     \ micron) lines, in 18 SINGS galaxies that have also been observed in CO\
+     \ (J = 1 - 0) emission through the Berkeley-Illinois-Maryland Array Surv\
+     ey of Nearby Galaxies (BIMA SONG).  We have developed unique software t\
+     hat creates spatially resolved maps of extremely faint and blended spec\
+     tral lines from Spitzer IRS LL data cubes.  Using our software, we prop\
+     ose to map H2 S(0) and H2 S(1) emission across each galaxy and use thes\
+     e two lines to model variations in the H2 excitation and mass across ea\
+     ch galaxy.  We will distinguish the H2 excitation mechanism as a functi\
+     on of position across each galaxy by comparing the H2 maps to CO emissi\
+     on (from SONG) and maps of mid-infrared spectral diagnostics such as th\
+     e [O IV](25.89 micron) line (a shock diagnostic) and the [S III](18.71 \
+     micron)-to-[S III](33.48 micron) line ratio (an HII region electron den\
+     sity diagnostic). We will also compare H2 spatial variations to UV and \
+     optical imagery (U, B, V, R,  and H(alpha)) for additional insight into\
+     \ the H2 excitation mechanisms and to correlate H2 properties with the a\
+     ges, masses, and star formation rates of stellar clusters and associati\
+     ons across the sample of SINGS galaxies.
+
+#------------------------- Extra Information --------------------------
---
+++ SINGS_ARCHIVAL_PROPOSAL/ll_data/proposal_spectrum.pro
@@ -0,0 +1,36 @@
+
+readcol, 'll1.tbl', ll1wave, ll1flux, ll1unc
+readcol, 'll2.tbl', ll2wave, ll2flux, ll2unc
+set_plot, 'ps'
+device, filename = 'LL_spec.ps', /LANDSCAPE
+plot, ll1wave,ll1flux, xstyle=1,ystyle=1, xrange=[14,39], yrange=[5, 60], xtitle = 'Wavelength (!7l!3m)', ytitle = 'MJy/sr', title = 'M51 Spectrum across the Long-Low Strip', thick = 3, charthick = 3, axis = 3
+oplot, ll2wave, ll2flux, thick = 3
+ll1 = fltarr(n_elements(ll1wave))
+ll2 = fltarr(n_elements(ll2wave))
+ll1 = ll1+47.5
+ll2 = ll2+45.5 
+oplot, ll1wave, ll1
+oplot, ll2wave, ll2
+xyouts, 33.6, 34, '[SIII](33.4 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 35, 50, '[SiII](34.8 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 28.3, 20, 'H2 S(0)(28.2 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 26, 18, '[FeII](25.98 !7l!3m)/[OIV](25.89 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 18.8, 15, '[SIII](18.71 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 17.2, 18, 'PAH(17.0 !7l!3m)/H2 S(1)(17.0 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 16.6, 15, 'PAH(16.4 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 15.7, 10, '[NeIII](15.5 !7l!3m)',  ALIGNMENT = 0.1, ORIENTATION = 90
+xyouts, 28, 48.5, 'LL1'
+xyouts, 17, 46.5, 'LL2'
+device, /close
+set_plot, 'x'
+;end
+
+ll_wave = [ll2wave,ll1wave]
+ll_flux = [ll2flux,ll1flux]
+ll_unc = [ll2unc,ll1unc]
+
+;end
+
+;fit = pahfit(ll_wave, ll_flux, ll_unc, REDSHIFT = 0, /PLOT_PROGRESS, XSIZE = 1000, YSIZE = 600)
+
+end
